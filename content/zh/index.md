348d20e65669e2dcb408b3c7ceb3c8c846064f81
changemyminds <david7717318@gmail.com>
2022-05-01 21:48:07 +0800

    feat(deploy): code Sense讀書會專用Gitlab Page

---
+++ content/zh/index.md
@@ -0,0 +1,12 @@
+---
+title: 讀書目錄
+description: 'CodeSense讀書會目錄'
+position: 1
+category: 基本介紹
+books:
+  - 持續交付2.0：實務導向的DevOps   
+---
+
+## 書籍
+
+<list :items="books"></list>
---
+++ nuxt.config.js
@@ -0,0 +1,39 @@
+import theme from '@nuxt/content-theme-docs'
+import pkg from './package.json';
+
+const routerBase = process.env.DEPLOY_ENV === 'GH_PAGES'
+  ? '/code-study/' : '/'
+
+export default theme({
+  router: {
+    base: routerBase
+  },
+  docs: {
+    primaryColor: '#00CD81'
+  },
+  generate: {
+    dir: 'public'
+  },
+  i18n: {
+    locales: () => [
+      {
+        code: 'zh',
+        iso: 'zh-TW',
+        file: 'zh-TW.js',
+        name: '中文'
+      }
+    ],
+    defaultLocale: 'zh',
+    vueI18n: {
+      fallbackLocale: 'zh'
+    },
+  },
+  css: ['~~/assets/css/main.css'],
+  colorMode: {
+    preference: 'dark',
+    fallback: 'light'
+  },
+  publicRuntimeConfig: {
+    clientVersion: pkg.version,
+  }
+})
