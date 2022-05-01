e689f393eaa11b92a399e81be8109109b4753876
changemyminds <david7717318@gmail.com>
2022-05-01 22:38:54 +0800

    feat(deploy): 新增gitlab page

---
+++ components/app/AppFooter.vue
@@ -0,0 +1,16 @@
+<template>
+  <footer class="h-16 relative w-full border-t dark:border-gray-800 bg-white dark:bg-gray-900 z-20 text-center flex items-center justify-center">
+      <div class="font-light">Code Sense</div>
+  </footer>
+</template>
+
+<script>
+
+export default {
+  computed: {
+    availableLocales() {
+      return this.$i18n.locales.filter((i) => i.code !== this.$i18n.locale);
+    },
+  },
+};
+</script>
