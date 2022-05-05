4b2e9340bde25965a01d34c433eb6899386b71d0
changemyminds <david7717318@gmail.com>
2022-05-05 21:23:32 +0800

    feat: 新增AppSubtitle、版本號更新
    1. 新增閱讀時間、閱讀字數、頁面瀏覽人數
    2. 新增npm version來更新版本號
    
    ISSUES CLOSED: #1

---
+++ components/app/AppSubtitle.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="flex flex-col">
+    <div v-if="document.subtitle">
+      <p class="text-gray-600 dark:text-gray-400">{{ document.subtitle }} </p>
+    </div>
+    <div>
+      <span class="text-sm">字數總計：{{ document.readingStats.words }} 個</span>
+      <span class="mx-1">|</span>
+      <span class="text-sm">
+        閱讀時長：{{ Math.ceil(document.readingStats.minutes) }} 分鐘
+      </span>
+      <span class="mx-1">|</span>
+      <span class="text-sm" id="busuanzi_container_page_pv">
+        <span>閱讀次數：</span>
+        <span id="busuanzi_value_page_pv"></span>
+        次
+      </span>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    document: {
+      type: Object,
+      required: true,
+    },
+  },
+};
+</script>
