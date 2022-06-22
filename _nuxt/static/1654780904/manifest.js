a9584ef84f5047c7d810d1b59b9fb3fe18cb144b
changemyminds <changemyminds@users.noreply.github.com>
2022-06-22 08:52:21 +0000

    deploy: 1a055fad84965bec58acf251b7abc72da1b5d5c8

--- _nuxt/static/1654780904/manifest.js
+++
@@ -1,1 +0,0 @@
-__NUXT_JSONP__("manifest.js", {routes:["\u002F","\u002Freleases","\u002Fcicd-2.0\u002Fchapter8","\u002Fcicd-2.0\u002Fchapter9","\u002Fweb-api\u002Fchapter2","\u002Fcicd-2.0\u002Fchapter7","\u002Fweb-api\u002Fchapter6","\u002Fweb-api\u002Fchapter5","\u002Fcicd-2.0\u002Fchapter5"]})
