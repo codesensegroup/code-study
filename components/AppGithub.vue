497061085357c0111540194e4663afabde61e6d2
changemyminds <david7717318@gmail.com>
2024-01-07 18:21:04 +0800

    feat: update document and home index
    - update home page and member page
    - move appendix00 to workshop folder and rename to liquibase
    - update README.md

---
+++ components/AppGithub.vue
@@ -0,0 +1,31 @@
+<template>
+  <NuxtLink
+    rel="noopener noreferrer"
+    title="github"
+    href="https://github.com/codesensegroup/code-study"
+    target="_blank"
+  >
+    <Icon name="mdi:github" />
+  </NuxtLink>
+</template>
+
+<style lang="ts" scoped>
+css({
+  a: {
+    display: 'flex',
+    color: '{color.gray.500}',
+    padding: '{space.4}',
+
+    '@dark': {
+      color: '{color.gray.400}'
+    },
+
+    '&:hover': {
+      color: '{color.gray.700}',
+      '@dark': {
+        color: '{color.gray.200}',
+      }
+    },
+  }
+})
+</style>
